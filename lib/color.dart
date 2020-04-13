bf09550bbc8d9accb3957ab41203064a7d7761f1
AliElZein24 <ali.r.elzein@gmail.com>
2020-04-13 12:45:16 +0300

    changecolor

---
+++ lib/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class ColorsChange extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( appBar: AppBar(
+              title: Text('Saved WordPairs',style: TextStyle(color:Colors.yellow),)));
+      
+  
+  }
+}
